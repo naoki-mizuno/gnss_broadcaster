79aa5c1f73b5295e8e5e77ada4d8b47f34dd5532
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-10-21 04:43:04 +0900

    Don't use '1.0 *' for casting to double
    This apparently introduces precision errors.

--- src/gnss_broadcaster/geo_pos_conv.py
+++ src/gnss_broadcaster/geo_pos_conv.py
@@ -136,70 +136,70 @@ class GeoPosConv:
         AW = 6378137.0  # Semi-major Axis
         FW = 1.0 / 298.257222101  # //298.257223563 //Geometrical flattening
 
-        Pe = 1.0 * sqrt(2.0 * FW - pow(FW, 2))
-        Pet = 1.0 * sqrt(pow(Pe, 2) / (1.0 - pow(Pe, 2)))
+        Pe = sqrt(2.0 * FW - pow(FW, 2))
+        Pet = sqrt(pow(Pe, 2) / (1.0 - pow(Pe, 2)))
 
-        PA = 1.0 * 1.0 + 3.0 / 4.0 * pow(Pe, 2) + 45.0 / 64.0 * pow(Pe, 4) + 175.0 / 256.0 * pow(Pe, 6) + 11025.0 \
+        PA = 1.0 + 3.0 / 4.0 * pow(Pe, 2) + 45.0 / 64.0 * pow(Pe, 4) + 175.0 / 256.0 * pow(Pe, 6) + 11025.0 \
                 / 16384.0 * pow(Pe, 8) + 43659.0 / 65536.0 * pow(Pe, 10) + 693693.0 / 1048576.0 * pow(Pe, 12) \
                 + 19324305.0 / 29360128.0 * pow(Pe, 14) + 4927697775.0 / 7516192768.0 * pow(Pe, 16)
 
-        PB = 1.0 * 3.0 / 4.0 * pow(Pe, 2) + 15.0 / 16.0 * pow(Pe, 4) + 525.0 / 512.0 * pow(Pe, 6) + 2205.0 / 2048.0 \
+        PB = 3.0 / 4.0 * pow(Pe, 2) + 15.0 / 16.0 * pow(Pe, 4) + 525.0 / 512.0 * pow(Pe, 6) + 2205.0 / 2048.0 \
             * pow(Pe, 8) + 72765.0 / 65536.0 * pow(Pe, 10) + 297297.0 / 262144.0 * pow(Pe, 12) + 135270135.0 \
             / 117440512.0 * pow(Pe, 14) + 547521975.0 / 469762048.0 * pow(Pe, 16)
 
-        PC = 1.0 * 15.0 / 64.0 * pow(Pe, 4) + 105.0 / 256.0 * pow(Pe, 6) + 2205.0 / 4096.0 * pow(Pe, 8) + 10395.0 \
+        PC = 15.0 / 64.0 * pow(Pe, 4) + 105.0 / 256.0 * pow(Pe, 6) + 2205.0 / 4096.0 * pow(Pe, 8) + 10395.0 \
             / 16384.0 * pow(Pe, 10) + 1486485.0 / 2097152.0 * pow(Pe, 12) + 45090045.0 / 58720256.0 * pow(Pe, 14) \
             + 766530765.0 / 939524096.0 * pow(Pe, 16)
 
-        PD = 1.0 * 35.0 / 512.0 * pow(Pe, 6) + 315.0 / 2048.0 * pow(Pe, 8) + 31185.0 / 131072.0 * pow(Pe, 10) \
+        PD = 35.0 / 512.0 * pow(Pe, 6) + 315.0 / 2048.0 * pow(Pe, 8) + 31185.0 / 131072.0 * pow(Pe, 10) \
             + 165165.0 / 524288.0 * pow(Pe, 12) + 45090045.0 / 117440512.0 * pow(Pe, 14) + 209053845.0 / 469762048.0 \
             * pow(Pe, 16)
 
-        PE = 1.0 * 315.0 / 16384.0 * pow(Pe, 8) + 3465.0 / 65536.0 * pow(Pe, 10) + 99099.0 / 1048576.0 * pow(Pe, 12) \
+        PE = 315.0 / 16384.0 * pow(Pe, 8) + 3465.0 / 65536.0 * pow(Pe, 10) + 99099.0 / 1048576.0 * pow(Pe, 12) \
             + 4099095.0 / 29360128.0 * pow(Pe, 14) + 348423075.0 / 1879048192.0 * pow(Pe, 16)
 
-        PF = 1.0 * 693.0 / 131072.0 * pow(Pe, 10) + 9009.0 / 524288.0 * pow(Pe, 12) + 4099095.0 / 117440512.0 * \
+        PF = 693.0 / 131072.0 * pow(Pe, 10) + 9009.0 / 524288.0 * pow(Pe, 12) + 4099095.0 / 117440512.0 * \
             pow(Pe, 14) + 26801775.0 / 469762048.0 * pow(Pe, 16)
 
-        PG = 1.0 * 3003.0 / 2097152.0 * pow(Pe, 12) + 315315.0 / 58720256.0 * pow(Pe, 14) + 11486475.0 / 939524096.0 \
+        PG = 3003.0 / 2097152.0 * pow(Pe, 12) + 315315.0 / 58720256.0 * pow(Pe, 14) + 11486475.0 / 939524096.0 \
             * pow(Pe, 16)
 
-        PH = 1.0 * 45045.0 / 117440512.0 * pow(Pe, 14) + 765765.0 / 469762048.0 * pow(Pe, 16)
+        PH = 45045.0 / 117440512.0 * pow(Pe, 14) + 765765.0 / 469762048.0 * pow(Pe, 16)
 
-        PI = 1.0 * 765765.0 / 7516192768.0 * pow(Pe, 16)
+        PI = 765765.0 / 7516192768.0 * pow(Pe, 16)
 
-        PB1 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PA
-        PB2 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PB / -2.0
-        PB3 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PC / 4.0
-        PB4 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PD / -6.0
-        PB5 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PE / 8.0
-        PB6 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PF / -10.0
-        PB7 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PG / 12.0
-        PB8 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PH / -14.0
-        PB9 = 1.0 * AW * (1.0 - pow(Pe, 2)) * PI / 16.0
+        PB1 = AW * (1.0 - pow(Pe, 2)) * PA
+        PB2 = AW * (1.0 - pow(Pe, 2)) * PB / -2.0
+        PB3 = AW * (1.0 - pow(Pe, 2)) * PC / 4.0
+        PB4 = AW * (1.0 - pow(Pe, 2)) * PD / -6.0
+        PB5 = AW * (1.0 - pow(Pe, 2)) * PE / 8.0
+        PB6 = AW * (1.0 - pow(Pe, 2)) * PF / -10.0
+        PB7 = AW * (1.0 - pow(Pe, 2)) * PG / 12.0
+        PB8 = AW * (1.0 - pow(Pe, 2)) * PH / -14.0
+        PB9 = AW * (1.0 - pow(Pe, 2)) * PI / 16.0
 
-        PS = 1.0 * PB1 * self.m_lat + PB2 * sin(2.0 * self.m_lat) + PB3 * sin(4.0 * self.m_lat) + PB4 \
+        PS = PB1 * self.m_lat + PB2 * sin(2.0 * self.m_lat) + PB3 * sin(4.0 * self.m_lat) + PB4 \
             * sin(6.0 * self.m_lat) + PB5 * sin(8.0 * self.m_lat) + PB6 * sin(10.0 * self.m_lat) + PB7 \
             * sin(12.0 * self.m_lat) + PB8 * sin(14.0 * self.m_lat) + PB9 * sin(16.0 * self.m_lat)
 
-        PSo = 1.0 * PB1 * self.m_PLato + PB2 * sin(2.0 * self.m_PLato) + PB3 * sin(4.0 * self.m_PLato) + PB4 \
+        PSo = PB1 * self.m_PLato + PB2 * sin(2.0 * self.m_PLato) + PB3 * sin(4.0 * self.m_PLato) + PB4 \
             * sin(6.0 * self.m_PLato) + PB5 * sin(8.0 * self.m_PLato) + PB6 * sin(10.0 * self.m_PLato) + PB7 \
             * sin(12.0 * self.m_PLato) + PB8 * sin(14.0 * self.m_PLato) + PB9 * sin(16.0 * self.m_PLato)
 
-        PDL = 1.0 * self.m_lon - self.m_PLo
-        Pt = 1.0 * tan(self.m_lat)
-        PW = 1.0 * sqrt(1.0 - pow(Pe, 2) * pow(sin(self.m_lat), 2))
-        PN = 1.0 * AW / PW
-        Pnn = 1.0 * sqrt(pow(Pet, 2) * pow(cos(self.m_lat), 2))
+        PDL = self.m_lon - self.m_PLo
+        Pt = tan(self.m_lat)
+        PW = sqrt(1.0 - pow(Pe, 2) * pow(sin(self.m_lat), 2))
+        PN = AW / PW
+        Pnn = sqrt(pow(Pet, 2) * pow(cos(self.m_lat), 2))
 
-        self.m_x = 1.0 * ((PS - PSo) + (1.0 / 2.0) * PN * pow(cos(self.m_lat), 2.0) * Pt * pow(PDL, 2.0) \
+        self.m_x = ((PS - PSo) + (1.0 / 2.0) * PN * pow(cos(self.m_lat), 2.0) * Pt * pow(PDL, 2.0) \
             + (1.0 / 24.0) * PN * pow(cos(self.m_lat), 4) * Pt * (5.0 - pow(Pt, 2) + 9.0 * pow(Pnn, 2) + 4.0 \
             * pow(Pnn, 4)) * pow(PDL, 4) - (1.0 / 720.0) * PN * pow(cos(self.m_lat), 6) * Pt * (-61.0 + 58.0 \
             * pow(Pt, 2) - pow(Pt, 4) - 270.0 * pow(Pnn, 2) + 330.0 * pow(Pt, 2) * pow(Pnn, 2)) * pow(PDL, 6) \
             - (1.0 / 40320.0) * PN * pow(cos(self.m_lat), 8) * Pt * (-1385.0 + 3111 * pow(Pt, 2) - 543 \
             * pow(Pt, 4) + pow(Pt, 6)) * pow(PDL, 8)) * Pmo
 
-        self.m_y = 1.0 * (PN * cos(self.m_lat) * PDL - 1.0 / 6.0 * PN * pow(cos(self.m_lat), 3) * (-1 + pow(Pt, 2) \
+        self.m_y = (PN * cos(self.m_lat) * PDL - 1.0 / 6.0 * PN * pow(cos(self.m_lat), 3) * (-1 + pow(Pt, 2) \
             - pow(Pnn, 2)) * pow(PDL, 3) - 1.0 / 120.0 * PN * pow(cos(self.m_lat), 5) * (-5.0 + 18.0 * pow(Pt, 2) \
             - pow(Pt, 4) - 14.0 * pow(Pnn, 2) + 58.0 * pow(Pt, 2) * pow(Pnn, 2)) * pow(PDL, 5) - 1.0 / 5040.0 * PN \
             * pow(cos(self.m_lat), 7) * (-61.0 + 479.0 * pow(Pt, 2) - 179.0 * pow(Pt, 4) + pow(Pt, 6)) * pow(PDL, 7)) \
