24029a0b79704cbb53a544d969279c51bdd093d9
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-10-27 02:39:42 +0900

    Add gps_antenna frame

--- nodes/gnss_broadcaster_node.py
+++ nodes/gnss_broadcaster_node.py
@@ -10,10 +10,11 @@ rospy.init_node('gnss_broadcaster')
 # Get params
 gps_origin_frame = rospy.get_param('~gps_origin_frame', 'gps_origin')
 gps_position_frame = rospy.get_param('~gps_position_frame', 'gps_position')
-gps_position_to_base = rospy.get_param('~gps_position_to_base', [[0, 0, 0], [0, 0, 0, 1]])
 gps_orientation_frame = rospy.get_param('~gps_orientation_frame', 'gps_orientation')
-gps_orientation_to_base = rospy.get_param('~gps_orientation_to_base', [[0, 0, 0], [0, 0, 0, 1]])
+gps_antenna_frame = rospy.get_param('~gps_antenna_frame', 'gps_antenna')
 gps_base_frame = rospy.get_param('~gps_base_frame', 'gps_base_link')
+gps_position_to_base = rospy.get_param('~gps_position_to_base', [[0, 0, 0], [0, 0, 0, 1]])
+gps_orientation_to_base = rospy.get_param('~gps_orientation_to_base', [[0, 0, 0], [0, 0, 0, 1]])
 
 tf_l = tf.TransformListener()
 
@@ -35,12 +36,11 @@ while not rospy.is_shutdown():
     tf_l.waitForTransform(gps_origin_frame, gps_orientation_frame, t, rospy.Duration(5))
     tf_o = tf_l.lookupTransform(gps_origin_frame, gps_orientation_frame, t)
 
-    # Add the offset of the antenna
-    gps_base = (
-        list(map(add, tf_p[0], gps_position_to_base[0])),
-        tf.transformations.quaternion_multiply(tf_o[1], gps_orientation_to_base[1])
-    )
-
-    # Broadcast gps_base_frame
+    # Broadcast gps_antenna (frame representing the pose of the GPS antenna) and gps_base_link
     tf_b = tf.TransformBroadcaster()
-    tf_b.sendTransform(gps_base[0], gps_base[1], rospy.Time.now(), gps_base_frame, gps_origin_frame)
+    t = rospy.Time.now()
+    # GPS antenna
+    tf_b.sendTransform(tf_p[0], tf_o[1], t, gps_antenna_frame, gps_origin_frame)
+    # Vehicle pose
+    # TODO: Use static transform broadcaster from tf2_ros
+    tf_b.sendTransform(gps_position_to_base[0], gps_orientation_to_base[1], t, gps_base_frame, gps_antenna_frame)
